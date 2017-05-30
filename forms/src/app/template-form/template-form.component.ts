9da58f90a58bf8809fab56046781f98ce35df254
prmorais13 <prmorais_13@hotmail.com>
2017-05-30 18:13:48 -0300

    Alterações no template-form

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -16,6 +16,10 @@ export class TemplateFormComponent implements OnInit {
   onSubmit(form){
     console.log(form);
     //console.log(this.usuario);
+
+    this.http.post('https://httpbin.org/post', JSON.stringify(form.value))
+      .map(res => res)
+      .subscribe(dados => console.log(dados));
   }
 
   constructor(private http: Http) { }
@@ -34,7 +38,7 @@ export class TemplateFormComponent implements OnInit {
     }
   }
 
-  consultaCEP(cep){
+  consultaCEP(cep, form){
     //Nova variável "cep" somente com dígitos.
     cep = cep.replace(/\D/g, '');
 
@@ -45,12 +49,53 @@ export class TemplateFormComponent implements OnInit {
 
       //Valida o formato do CEP.
       if(validacep.test(cep)) {
+
+        this.resetaDadosForm(form);
         //this.http.get("//viacep.com.br/ws/"+ cep +"/json/")
         this.http.get(`//viacep.com.br/ws/${cep}/json/`)
             .map(dados => dados.json())
-            .subscribe(dados => console.log(dados));
+            .subscribe(dados => this.populaDadosForm(dados, form));
       }
     }
+  }
+
+  populaDadosForm(dados, formulario){
+    /*formulario.setValue({
+      nome: formulario.value.nome,
+      email: formulario.value.email,
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep ,
+        numero: "" ,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });*/
+
+    formulario.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        //cep: dados.cep ,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm(formulario){
+    formulario.form.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
 }
