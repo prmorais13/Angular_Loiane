a5ab7d6c3907415951531cf80fc4157cef3d2bd1
prmorais13 <prmorais_13@hotmail.com>
2017-06-01 17:24:19 -0300

    Form reativo 2

--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DataFormComponent } from './data-form.component';
 import { ReactiveFormsModule } from "@angular/forms";
+
+import { DataFormComponent } from './data-form.component';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
   declarations: [
     DataFormComponent
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -4,19 +4,17 @@ import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 
 import { TemplateFormComponent } from './template-form.component';
-import { FormDebugComponent } from './../form-debug/form-debug.component';
-import { CampoControlErroComponent } from './../campo-control-erro/campo-control-erro.component';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SharedModule
   ],
   declarations: [
-    TemplateFormComponent,
-    FormDebugComponent,
-    CampoControlErroComponent
+    TemplateFormComponent
   ]
 })
 export class TemplateFormModule { }
