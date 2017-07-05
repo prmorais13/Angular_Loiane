7ed28617068e9cc99fb682145fde80489c929cd7
prmorais13 <prmorais_13@hotmail.com>
2017-07-05 16:37:23 -0300

    Atualização aula 99

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -32,62 +32,81 @@ export class DataFormComponent implements OnInit {
         rua: [null, Validators.required],
         bairro: [null, Validators.required],
         cidade: [null, Validators.required],
-        estado: [null,Validators.required]
+        estado: [null, Validators.required]
       })
     });
   }
 
-  onSubmit(){
-    console.log(this.formulario);
-    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
-      .map(res => res)
-      .subscribe(dados => {
-        console.log(dados);
-        //this.formulario.reset();
-        //this.resetar();
-      },
-      (erro: any) => alert('Erro'));
+  onSubmit() {
+
+    if (this.formulario.valid){
+      console.log(this.formulario);
+      this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+        .map(res => res)
+        .subscribe(dados => {
+          console.log(dados);
+          // this.formulario.reset();
+          // this.resetar();
+        },
+        (erro: any) => alert('Erro'));
+    } else {
+      console.log('Formulário inválido!');
+      this.verificaValidacoesForm(this.formulario);
+    }
   }
 
-  resetar(){
+  verificaValidacoesForm(formGroup: FormGroup) {
+     Object.keys(formGroup.controls).forEach(campo => {
+        console.log(campo);
+        const controle = formGroup.get(campo);
+        controle.markAsDirty();
+        if (controle instanceof FormGroup) {
+          this.verificaValidacoesForm(controle);
+        }
+
+      });
+  }
+
+  resetar() {
     this.formulario.reset();
   }
 
-  verificaValidTaouched(campo: string){
-   return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+  verificaValidTaouched(campo: string) {
+   return !this.formulario.get(campo).valid && (this.formulario.get(campo).touched ||
+          this.formulario.get(campo).dirty);
   }
 
-  verificaEmailInvalido(){
-    let campoEmail = this.formulario.get('email');
+  verificaEmailInvalido( ) {
+    const campoEmail = this.formulario.get('email');
 
-    if(campoEmail.errors){
+    if (campoEmail.errors){
       return campoEmail.errors['email'] && campoEmail.touched;
     }
   }
 
-  aplicaCssErro(campo: string){
+  aplicaCssErro(campo: string) {
     return {
       'has-error': this.verificaValidTaouched(campo),
       'has-feedback': this.verificaValidTaouched(campo)
     }
   }
 
-  consultaCEP(){
+  consultaCEP() {
     let cep = this.formulario.get('endereco.cep').value;
 
-    //Nova variável "cep" somente com dígitos.
+    // Nova variável "cep" somente com dígitos.
     cep = cep.replace(/\D/g, '');
 
-    //Verifica se campo cep possui valor informado.
-    if (cep != "") {
-      //Expressão regular para validar o CEP.
-      var validacep = /^[0-9]{8}$/;
+    // Verifica se campo cep possui valor informado.
+    if (cep !== '') {
+      // Expressão regular para validar o CEP.
+      const validacep = /^[0-9]{8}$/;
 
-      //Valida o formato do CEP.
-      if(validacep.test(cep)) {
+      // Valida o formato do CEP.
+      if (validacep.test(cep)) {
 
         this.resetaDadosForm();
-        //this.http.get("//viacep.com.br/ws/"+ cep +"/json/")
+        // this.http.get("//viacep.com.br/ws/"+ cep +"/json/")
         this.http.get(`//viacep.com.br/ws/${cep}/json/`)
             .map(dados => dados.json())
             .subscribe(dados => this.populaDadosForm(dados));
@@ -95,7 +114,7 @@ export class DataFormComponent implements OnInit {
     }
   }
 
-  populaDadosForm(dados){
+  populaDadosForm(dados) {
     /*formulario.setValue({
       nome: formulario.value.nome,
       email: formulario.value.email,
@@ -113,7 +132,7 @@ export class DataFormComponent implements OnInit {
     this.formulario.patchValue({
       endereco: {
         rua: dados.logradouro,
-        //cep: dados.cep ,
+        // cep: dados.cep ,
         complemento: dados.complemento,
         bairro: dados.bairro,
         cidade: dados.localidade,
@@ -122,7 +141,7 @@ export class DataFormComponent implements OnInit {
     });
   }
 
-  resetaDadosForm(){
+  resetaDadosForm() {
     this.formulario.patchValue({
       endereco: {
         rua: null,
